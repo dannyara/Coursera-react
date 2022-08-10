e7653b58defd89423f2dc5d6bf77a61578dc8115
daraujo-etg <daraujo@essextec.com>
2022-08-10 12:41:21 -0400

    refactoring of functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,35 +2,39 @@ import {Media, Card, CardImg, CardImgOverlay, CardTitle, CardText, CardBody, Lis
 import {useState} from "react";
 
 
+function RenderComments({comment, date}){
+    return (
+        <ul key={comment.id} className={'list-unstyled'}>
+            <li>
+                <div> {comment.comment}</div>
+                <div> -- {comment.author}, {date}</div>
+            </li>
+        </ul>
+    )
+}
+
 function DishDetail({dish}) {
 
     const renderDishes =() => {
         return(
-                <Card>
-                    <CardImg width='100%' object src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle> {dish.name}</CardTitle>
-                        <CardText> {dish.description}</CardText>
-                    </CardBody>
-                </Card>
+            <Card>
+                <CardImg width='100%' object src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle> {dish.name}</CardTitle>
+                    <CardText> {dish.description}</CardText>
+                </CardBody>
+            </Card>
 
         )
     }
 
 
     if (dish != null) {
-        const renderComments = dish.comments.map((comment) => {
+        const getComments = dish.comments.map((comment) => {
             if(dish.comments!= null){
                 let options = { year: 'numeric', month: 'short', day: '2-digit'};
                 const date = new Intl.DateTimeFormat('en-US', options).format(new Date(Date.parse(comment.date)))
-                return(
-                    <ul key={comment.id} className={'list-unstyled'}>
-                        <li>
-                            <div> {comment.comment}</div>
-                            <div> -- {comment.author}, {date}</div>
-                        </li>
-                    </ul>
-                )
+                return <RenderComments comment={comment} date={date}/>
             }
             else{ return <div> </div>}
         })
@@ -43,7 +47,7 @@ function DishDetail({dish}) {
                     </div>
                     <div  className='col-12 col-md-5 m-1'>
                         <h4> Comments</h4>
-                        {renderComments}
+                        {getComments}
                     </div>
                 </div>
             </div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,21 @@
 import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
 
-function Menu({dishes, onClick}) {
+function RenderMenu({dish, onClick}) {
+    return (
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width='100%' object src={dish.image} alt={dish.name} />
+            <CardImgOverlay body className='ml-5'>
+                <CardTitle heading> {dish.name} </CardTitle>
+            </CardImgOverlay>
+        </Card>
+    )}
 
+
+const Menu = ({dishes, onClick}) => {
     const menu = dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-2">
-                    <Card onClick={() => onClick(dish.id)}>
-                        <CardImg width='100%' object src={dish.image} alt={dish.name} />
-                        <CardImgOverlay body className='ml-5'>
-                            <CardTitle heading> {dish.name} </CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <RenderMenu dish={dish} onClick={onClick} />
                 </div>
             )
         }
